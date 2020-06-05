6654c697e6208397ffb6426ab03b0a3d5adfc747
sheriff-kurdi <sheriff.kurdi@gmail.com>
2020-06-05 16:35:49 +0200

    Blogs

---
+++ YallaMedia/YallaMedia/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using YallaMedia.Models;
+
+namespace YallaMedia.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
