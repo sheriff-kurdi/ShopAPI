a808f3c381ea13f2a62b5afa3db78e20dd82f3b7
sheriff-kurdi <sheriff.kurdi@gmail.com>
2020-06-19 01:46:07 +0200

    init

---
+++ RiskAndRequiredRateOfReturn/RiskAndRequiredRateOfReturn/Models/ShareRateOfReturnVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiskAndRequiredRateOfReturn.Models
+{
+    public class ShareRateOfReturnVM
+    {
+        [Required]
+        public float CashFlowReceved { get; set; }
+        [Required]
+        public float ValueAtBeginning { get; set; }
+        [Required]
+        public float ValueAtEnding { get; set; }
+
+
+    }
+}
---
+++ SimpleInvoice/SimpleInvoice/Models/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleInvoice.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string ItemName { get; set; }
+        public string UnitName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double Total { get; set; }
+        public double Discount { get; set; }
+        //total after discount and tax
+        public double Net { get; set; }
+
+        //total after discount
+        //Fixed
+        //public double Tax { get; set; }
+
+    }
+}
